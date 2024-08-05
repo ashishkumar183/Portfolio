6eda7279a5adda2fe7adae465eaac0321d809034
ASHISH KUMAR <ashishkumar183183@gmail.com>
2024-08-05 23:26:42 +0530

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './Home/Home';
 import About from './about/About';
 import Skill from './Skills/Skill';
 import Qualification from './qualification/Qualification';
-import Technology from './technology/Technology';
+// import Technology from './technology/Technology';
 import Contact from './contact/Contact';
 import Projects from './project/Project';
 function App() {
